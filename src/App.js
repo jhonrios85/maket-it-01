86fcceefe5d685e34f854f749256e3ea0521e62a
Jhon Rios <jhonrios85@gmail.com>
2019-05-16 18:16:26 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi</h1>
+      <p></p>
+    </div>
+  );
+}
+
+export default App;
